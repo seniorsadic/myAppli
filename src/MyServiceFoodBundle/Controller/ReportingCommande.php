d961c9ebd50005b0dcbc55a30833d7ec9b50c84a
topchico <33789800+topchico@users.noreply.github.com>
2018-06-02 15:35:35 +0000

    Report commande initial

---
+++ src/MyServiceFoodBundle/Controller/ReportingCommande.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MyServiceFoodBundle\Controller;
+
+use MyServiceFoodBundle\Entity\Article;
+use MyServiceFoodBundle\Entity\Commande_article;
+use MyServiceFoodBundle\Entity\Commande;
+use MyServiceFoodBundle\Entity\Categorie;
+use MyServiceFoodBundle\Entity\Employe;
+use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\Request;
+
+class ReportingCommande extends FOSRestController
+{
+
+    /**
+     * @Rest\Post("/article/")
+     */
+    public function getReportCommande()
+    {
+        $em=$this->getDoctrine()->getRepository('MyServiceFoodBundle:Commande_article');
+        $query=$em->createQueryBuilder('ca')
+            ->join('ca.id_commannde','c')
+            ->join('ca.id_article','a')
+            ->join('a.id_categorie','ct')
+            ->join('c.id_serveur','e')
+            ->addSelect('ca')
+            ->addSelect('c')
+            ->addSelect('a')
+            ->addSelect('ct')
+            ->addSelect('e')
+            ->groupBy('c.numero', 'a.designation','ca.quantite','ca.prix_unitaire', 'ca.prix_total','c.date','e.nom','e.prenom')
+            ->select('c.numero', 'a.designation','ca.quantite','ca.prix_unitaire', 'ca.prix_total','c.date','e.nom','e.prenom', sum(ca.prix_total), count(0))
+            ->getQuery();
+        $restresult=$query->getResult();
+
+        return $restresult;
+
+
+    }
+}
